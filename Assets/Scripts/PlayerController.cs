4e56b16dfa2a78f01d6c35c0b4db7b1c4d6ea13e
Nông Thanh Chiến <chiennt2@rikkeisoft.com>
2023-12-05 15:51:11 +0700

    Add environment and physics

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,18 +6,22 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public InputAction MoveAction;
+    Rigidbody2D rigidbody2d;
+    Vector2 move;
     [SerializeField] float speed = 5.0f;
     void Start()
     {
         MoveAction.Enable();
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
-
-    // Update is called once per frame
     void Update()
     {
-        Vector2 move = MoveAction.ReadValue<Vector2>();
+        move = MoveAction.ReadValue<Vector2>();
         Debug.Log(move);
+    }
+    void FixedUpdate()
+    {
         Vector2 position = (Vector2)transform.position + move * speed * Time.deltaTime;
-        transform.position = position;
+        rigidbody2d.MovePosition(position);
     }
 }
